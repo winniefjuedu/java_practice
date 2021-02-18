b7bf6260972380763f9aa645c7d97d0f8b5ddfe2
winniefjuedu <75404114+winniefjuedu@users.noreply.github.com>
2021-02-18 15:44:12 +0800

    Object & Class

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -2,56 +2,32 @@ public class JavaProject {
     // SDK: amazon corretto
     // IDE: jetbrains
     public static void main (String[] args){
-        byte numberOfSeats = 5;
-        byte numberOfDoors = 3;
-        byte numberOfVehicleOwners = 1;
-        byte emissionSticker = 4;
+        Byte numberOfSeats = 5;
+        Short horsePower = 392;
+        Integer price = 14999;
+        Long registrationNumber = 23452352345245234L;
 
-        short power = 362;
-        short horsePower = 492;
-        short co2Emission = 333;
-        short cubicCapacity = 6417;
+        Float fuelConsumptionUrban = 15.5F;
+        Double fuelConsumptionPreciseAverage = 15.235252345;
 
-        int price = 29999;
-        int mileage = 14999;
+        Boolean isDamaged = true;
+        Character energyEfficiencyCategory = 'G';
 
-        long registrationNumber = 134513532145234553L;
+        String carType = "Dodge Challenger SRT 392";
+        String carTypeWithNewKeyword = new String("Dodge Challenger SRT 392");
 
-        float fuelConsumptionCombined = 15.5F;
-        float fuelConsumptionUrban = 21.4F;
-        float fuelConsumptionExtraUrban = 13.6F;
-
-        double fuelConsumptionPreciseAverage = 153.2452345234525E-1;
-
-        boolean isDamaged = true;
-        char energyEfficiencyCategory = 'G';
-
-
-        System.out.println("2018 Dodge Challenger SRT 392");
-        System.out.println("Price: €" + price);
-        System.out.println("Mileage: " + mileage + "km");
-        System.out.println("The car is damaged: " + isDamaged);
-        System.out.println("Registration number: " + registrationNumber);
-        System.out.println("Cubic capacity: " + cubicCapacity);
-        System.out.println("Power: " + power + "kW(" + horsePower + "hp)");
-        System.out.println("CO2 emission: " + co2Emission + "g/km");
-        System.out.println("Emission sticker: " + emissionSticker + " ( Energy efficiency: " + energyEfficiencyCategory + ")");
-        System.out.println("Combined fuel consumption: " + fuelConsumptionCombined + "l/100km");
-        System.out.println("Urban fuel consumption: " + fuelConsumptionUrban + "l/100km");
-        System.out.println("Extra urban fuel consumption: " + fuelConsumptionExtraUrban + "l/100km");
-        System.out.println("Precise combined fuel consumption: " + fuelConsumptionPreciseAverage + "l/100km");
-        System.out.println("Number of previous owners: " + numberOfVehicleOwners);
-        System.out.println("Number of doors: " + numberOfDoors);
         System.out.println("Number of seats: " + numberOfSeats);
-
-        System.out.println("Conversion");
-        short newNumberOfSeats = numberOfSeats;
-        System.out.println("New value of number of seats: " + newNumberOfSeats);
-
-        int newMileage = mileage;
-        System.out.println("New value of mileage: " + newMileage);
-
-        byte newPower = (byte) power;
-        System.out.println("New value of power: " + newPower);
+        System.out.println("Horsepower: " + horsePower + "hp");
+        System.out.println("Price: €" + price.floatValue() );
+        System.out.println("Registration number: " + registrationNumber);
+        System.out.println("Urban fuel consumption: " + fuelConsumptionUrban + "l/100km");
+        System.out.println("Precise average fuel consumption: " + fuelConsumptionPreciseAverage.intValue() + "l/100km");
+        System.out.println("The car is damaged: " + isDamaged);
+        System.out.println("Energy efficiency category: " + energyEfficiencyCategory);
+        System.out.println("Car model: " + carType);
+        System.out.println("Car model with uppercase: " + carType.toUpperCase());
+        System.out.println("Car model with lowercase: " + carType.toLowerCase());
+        System.out.println("Car model with new keyword: " + carTypeWithNewKeyword);
+        System.out.println("carType variable equals to carTypeWithNewKeyword: " + carTypeWithNewKeyword.equals(carType));
     }
 }
